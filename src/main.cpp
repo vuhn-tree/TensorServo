ae175a93ed5c34d2cf7f834fb9bd17c9c4a361af
Richard Vu <rqvu.mail@gmail.com>
2023-02-21 00:15:01 -0800

    added tests in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <M5Core2.h>
+
+int counter = 0;
 
 void setup() {
+  M5.begin(true, true, true, true, kMBusModeInput);
   // put your setup code here, to run once:
+
+  M5.Lcd.setCursor(0, 0, 4);
+  M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
+  M5.Lcd.print("Servo TEST");
+  M5.Lcd.clear();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  M5.Lcd.setCursor(0, 0, 4);
+  M5.Lcd.println(counter);
+  delay(5000);
+  counter++;
 }
