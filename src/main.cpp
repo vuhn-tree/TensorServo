65e4bf4827070703b25162b1a01a39eea95dc2de
Richard Vu <rqvu.mail@gmail.com>
2023-02-21 00:16:21 -0800

    removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@ int counter = 0;
 
 void setup() {
   M5.begin(true, true, true, true, kMBusModeInput);
-  // put your setup code here, to run once:
-
   M5.Lcd.setCursor(0, 0, 4);
   M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
   M5.Lcd.print("Servo TEST");
@@ -14,8 +12,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
   M5.Lcd.setCursor(0, 0, 4);
   M5.Lcd.println(counter);
   delay(5000);
