23b0d25142bf7fa2fa40e00f931a770154b2483b
Richard Vu <rqvu.mail@gmail.com>
2023-02-23 23:47:32 -0800

    used string buffer in place of string

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,15 @@ void setup() {
   M5.begin(true, true, true, true, kMBusModeInput);
   M5.Lcd.setCursor(0, 0, 4);
   M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
-  M5.Lcd.print("Servo TEST");
+  M5.Lcd.print("Servo Counter");
   M5.Lcd.clear();
 }
 
 void loop() {
   M5.Lcd.setCursor(0, 0, 4);
-  M5.Lcd.println(counter);
+  char buf[100];
+  sprintf(buf, "counter: %04d", counter);
+  M5.Lcd.println(buf);
   delay(5000);
   counter++;
 }
