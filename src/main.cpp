2fe6175a96ff4961d86fe4024baf8a76ed03d92c
Richard Vu <rqvu.mail@gmail.com>
2023-02-24 18:02:43 -0800

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
 // #include <Arduino.h>
 #include <M5Core2.h>
-
 #include <Wire.h>
+
 #include "Adafruit_PWMServoDriver.h"
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire1);
 
 #define SERVOMIN 102  // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 512  // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN 500     // This is the rounded 'minimum' microsecond length based on the minimum pulse of 102
-#define USMAX 2500    // This is the rounded 'maximum' microsecond length based on the maximum pulse of 512
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define USMIN \
+  500  // This is the rounded 'minimum' microsecond length based on the minimum
+       // pulse of 102
+#define USMAX \
+  2500  // This is the rounded 'maximum' microsecond length based on the maximum
+        // pulse of 512
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
 
-int sensorPin = 36;         // set the input pin for the potentiometer.
-int last_sensorValue = 100; // Stores the value last read by the sensor.
-int cur_sensorValue = 0;    // Stores the value currently read by the sensor.
+int sensorPin = 36;          // set the input pin for the potentiometer.
+int last_sensorValue = 100;  // Stores the value last read by the sensor.
+int cur_sensorValue = 0;     // Stores the value currently read by the sensor.
 
 void setup() {
   M5.begin(true, true, true, true, kMBusModeInput);
   /* kMBusModeOutput, powered by USB or Battery
-   kMBusModeInput, powered by outside input need to fill in this Otherwise M5Core2 will not work properly
+   kMBusModeInput, powered by outside input need to fill in this Otherwise
+   M5Core2 will not work properly
    */
   Wire1.begin(21, 22);
   pwm.begin();
   pwm.setPWMFreq(50);
   Serial.begin(9600);
 
-  pinMode(sensorPin, INPUT); // Sets the specified pin to input mode.
+  pinMode(sensorPin, INPUT);  // Sets the specified pin to input mode.
 
   M5.Lcd.setCursor(100, 0, 4);
   M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
@@ -36,18 +41,18 @@ void setup() {
 }
 
 void setServoPulse(uint8_t n, double pulse) {
-    double pulselength;
-    pulselength = 1000000; // 1,000,000 us per second
-    pulselength /= 50;     // 50 Hz
-    Serial.print(pulselength);
-    Serial.println(" us per period");
-    pulselength /= 4096; // 12 bits of resolution
-    Serial.print(pulselength);
-    Serial.println(" us per bit");
-    pulse *= 1000;
-    pulse /= pulselength;
-    Serial.println(pulse);
-    pwm.setPWM(n, 0, pulse);
+  double pulselength;
+  pulselength = 1000000;  // 1,000,000 us per second
+  pulselength /= 50;      // 50 Hz
+  Serial.print(pulselength);
+  Serial.println(" us per period");
+  pulselength /= 4096;  // 12 bits of resolution
+  Serial.print(pulselength);
+  Serial.println(" us per bit");
+  pulse *= 1000;
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
 }
 
 void servo_angle_write(uint8_t n, int Angle) {
@@ -58,14 +63,13 @@ void servo_angle_write(uint8_t n, int Angle) {
 
 void loop() {
   // mapped to pot
-  cur_sensorValue = analogRead(sensorPin); // read the value from the sensor.
+  cur_sensorValue = analogRead(sensorPin);  // read the value from the sensor.
   const int normalVal = map(cur_sensorValue, 0, 4096, 0, 180);
 
   M5.Lcd.setCursor(100, 100, 4);
   char buf[20];
   sprintf(buf, "%d%%", cur_sensorValue);
   M5.Lcd.print(buf);
-  
 
   // first sensor value
   servo_angle_write(0, normalVal);
